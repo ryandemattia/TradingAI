ceb059ec940a98a828f24505efdb720afb05a347
nick williams <nick5664@outlook.com>
2018-08-25 17:00:34 -0600

    history

--- polo_hist.py
+++ polo_hist.py
@@ -11,14 +11,17 @@ from datetime import date, timedelta, datetime
 def pull_polo():
     polo = Poloniex()
     coins = polo.returnTicker()
-    tickLen = '3600'
+    tickLen = '7200'
     start = datetime.today() - timedelta(365) 
-    start = str(time.mktime(start.timetuple()))
-    with open('polopredict.txt', 'w') as f:
-        f.write('date,coin,lastPrice,nextPrice,accuraccy,returns\n')
-        for coin in coins:
-            if coin[:3] == 'BTC':
-                hist = requests.get('https://poloniex.com/public?command=returnChartData&currencyPair='+coin+'&start='+start+'&end=9999999999&period='+tickLen)
-                print(hist)
+    start = str(int(start.timestamp()))
+    for coin in coins:
+        if coin[:3] == 'BTC':
+            hist = requests.get('https://poloniex.com/public?command=returnChartData&currencyPair='+coin+'&start='+start+'&end=9999999999&period='+tickLen)
+            try:
+                frame = pd.DataFrame(hist.json())
+                print(frame.head())
+                frame.to_csv(coin+"_hist.txt", encoding="utf-8")
+            except:
+                print("error reading json")
 
 pull_polo()
