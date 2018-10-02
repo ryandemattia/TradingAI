94c0332321a48f41a531e1892232347b57b750ef
nick williams <nick5664@outlook.com>
2018-10-01 22:26:47 -0600

    full join with moon dist frame and hist ticker

--- hist_service.py
+++ hist_service.py
@@ -6,22 +6,36 @@ import numpy as np
 from poloniex import Poloniex
 from datetime import date, timedelta, datetime 
 import os
+from ephemGravityWrapper import gravity as gbaby
 
 class HistWorker:
 
     currentHists = {}
     
-    def get_hist_files():
+    def get_hist_files(self):
         histFiles = os.listdir(os.path.join(os.path.dirname(__file__), 'histories'))
         return histFiles
 
-    def get_data_frame(fname):
+    def get_data_frame(self, fname):
         frame = pd.read_csv('./histories/'+fname)
         return frame
         
-    def get_file_symbol(f):
+    def get_file_symbol(self, f):
         f = f.split("_", 2)
         return f[1]
+    def get_data_for_astro(self):
+        data = {}
+        dates = []
+        md = []
+        l_of_frame = len(self.currentHists['DASH']['date'])
+        for snoz in range(0, l_of_frame):
+            new_date = datetime.utcfromtimestamp((self.currentHists['DASH']['date'][snoz])).strftime('%Y-%m-%d  %H:%M:%S')
+            dates.append(new_date)
+            md.append(gbaby.planet_dist(new_date, 'moon'))
+        
+        data = {'dates': dates, 'moon_dist': md}
+        data = pd.DataFrame.from_dict(data)
+        return data
 
     def pull_polo():
         polo = Poloniex()
@@ -35,16 +49,16 @@ class HistWorker:
                 try:
                     frame = pd.DataFrame(hist.json())
                     print(frame.head())
-                    frame.to_csv(coin+"_hist.txt", encoding="utf-8")
+                    frame.to_csv("./histories/"+coin+"_hist.txt", encoding="utf-8")
                 except:
                     print("error reading json")
 
     
     def combine_frames(self):
-        fileNames = get_hist_files()
+        fileNames = self.get_hist_files()
         for x in range(0,len(fileNames)):
-            df = get_data_frame(fileNames[x])
-            col_prefix = get_file_symbol(fileNames[x])
+            df = self.get_data_frame(fileNames[x])
+            col_prefix = self.get_file_symbol(fileNames[x])
             #df.rename(columns = lambda x: col_prefix+'_'+x, inplace=True)
             self.currentHists[col_prefix] = df
         '''
@@ -58,3 +72,5 @@ class HistWorker:
         return
 
     
+hs = HistWorker()
+print(hs.get_data_for_astro())
