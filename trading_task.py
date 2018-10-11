9ee33c8979a6ef6d80bede33654cd88ad16f6de0
nick williams <nick5664@outlook.com>
2018-10-11 01:01:02 -0600

    gonna run with pureples instead of peas for visualization and ease of use, may switch back when im more familiar with how to pass the task domain to my network

--- trading_task.py
+++ trading_task.py
@@ -12,27 +12,52 @@ import numpy as np
 from hist_service import HistWorker
 from crypto_evolution import CryptoFolio
 # Local
+from pureples import neat
+import neat.nn
+import cPickle as pickle
+from pureples.shared.substrate import Substrate
+from pureples.shared.visualize import draw_net
+from pureples.es_hyperneat.es_hyperneat import ESNetwork
 
+'''
 from peas.peas.methods.hyperneat import HyperNEATDeveloper, Substrate
 #import peas.peas.networks.rnn.NeuralNetwork as nn
 from peas.peas.methods.neat import NEATPopulation, NEATGenotype
 from peas.peas.methods.evolution import SimplePopulation
-
+'''
+'''
+will come back to this, trying to get something up and running
+tonight so gonna make a quick n dirty purples experiment
+''' 
+#SQUAD
 class TradingTask:
 
-    EPSILON = 1e-100
+    #EPSILON = 1e-100
 
     start_idx = 0
     highest_returns = 0
     portfolio_list = []
+    # ES-HyperNEAT specific parameters.
+    params = {"initial_depth": 0, 
+            "max_depth": 1, 
+            "variance_threshold": 0.03, 
+            "band_threshold": 0.3, 
+            "iteration_level": 1,
+            "division_threshold": 0.5, 
+            "max_weight": 5.0, 
+            "activation": "sigmoid"}
 
-
+    # Config for CPPN.
+    config = neat.config.Config(neat.genome.DefaultGenome, neat.reproduction.DefaultReproduction,
+                                neat.species.DefaultSpeciesSet, neat.stagnation.DefaultStagnation,
+                                'config_cppn_xor')
     def __init__(self):
         self.hs = HistWorker()
         self.end_idx = len(self.hs.currentHists["DASH"])
         self.but_target = .1
         self.inputs = self.hs.hist_shaped.shape[0]*self.hs.hist_shaped[0].shape[1]
         self.outputs = self.hs.hist_shaped.shape[0] # times by three for buy | sell | hodl(pass)
+        self.sub = Substrate(self.inputs, self.outputs)
         #self.port = CryptoFolio(1)
 
     
