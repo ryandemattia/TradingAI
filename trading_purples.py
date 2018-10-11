235e747765e3fcc5f6572bf03379444ecb568a10
Nicholas Williams <nick5664@outlook.com>
2018-10-11 22:43:12 +0000

    using pureples for this one

--- trading_purples.py
+++ trading_purples.py
@@ -47,5 +47,52 @@ class PurpleTrader:
         self.end_idx = len(self.hs.currentHists["DASH"])
         self.but_target = .1
         self.inputs = self.hs.hist_shaped.shape[0]*self.hs.hist_shaped[0].shape[1]
-        self.outputs = self.hs.hist_shaped.shape[0] 
+        self.outputs = self.hs.hist_shaped.shape[0]
+        self.subStrate = Substrate()
+        
+        
+    def set_portfolio_keys(self, folio):
+        for k in self.hs.currentHists.keys():
+            folio.ledger[k] = 0
+
+    def get_one_bar_input_2d(self, end_idx):
+        active = {}
+        for x in range(0, self.outputs):
+            active[x] = self.hs.hist_shaped[x][end_idx]
+        return active
+
+    def evaluate(self, network, verbose=False):
+        portfolio = CryptoFolio(1)
+        active = {}
+        result = {}
+        if not isinstance(network, NeuralNetwork):
+            network = NeuralNetwork(network)
+        for z in range(0, 14):
+            '''
+            if(z == 0):
+                old_idx = 1
+            else:
+                old_idx = z * 5
+            new_idx = (z + 1) * 5
+            '''
+            active = self.get_one_bar_input_2d(z)
+            results[z] = network.feed(active)
+
+        for i in range(0, 14):
+            out = results[i]
+            for x in range(0, self.outputs):
+                sym = self.hs.coin_dict[x]
+                if(out[x] == 1.0):
+                    portfolio.buy_coin(sym, self.hs.currentHists[sym][x]['close'])
+                elif(out[x] == 0.0):
+                    portfolio.sell_coin(sym)
+        end_ts = self.hs.hist_shaped[0][14][0]
+        result_val = portfolio.get_total_btc_value(int(end_ts))
+        print(results)
+        if(results > self.highest_returns):
+            self.highest_returns = results
+        return results
+
+    def solve(self, network):
+        return self.evaluate(network) >= self.highest_returns
         
