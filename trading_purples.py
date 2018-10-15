4291a3d6e34c57b937d7d6c22088f078e7bf86b7
Nicholas Williams <nick5664@outlook.com>
2018-10-15 20:57:25 +0000

    back up and running yeeeeeeeeeeehahahahahahahaha

--- trading_purples.py
+++ trading_purples.py
@@ -54,7 +54,7 @@ class PurpleTrader:
             for ix2 in range(len(self.hs.hist_shaped[0][0])-1):
                 self.in_shapes.append((ix, ix2))
         self.subStrate = Substrate(self.in_shapes, self.out_shapes)
-        self.epoch_len = 24
+        self.epoch_len = 48
         
     def set_portfolio_keys(self, folio):
         for k in self.hs.currentHists.keys():
@@ -140,7 +140,7 @@ def run_pop(task, gens):
 # If run as script.
 if __name__ == '__main__':
     task = PurpleTrader()
-    winner = run_pop(task, 34)[0]
+    winner = run_pop(task, 21)[0]
     print('\nBest genome:\n{!s}'.format(winner))
 
     # Verify network output against training data.
