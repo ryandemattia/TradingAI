50debc67df4a5fd9ee23ff40ee1baf52607c23ae
Nicholas Williams <nick5664@outlook.com>
2018-11-20 05:52:38 +0000

    k

--- trading_purples.py
+++ trading_purples.py
@@ -145,7 +145,7 @@ def run_pop(task, gens):
 # If run as script.
 if __name__ == '__main__':
     task = PurpleTrader(5)
-    winner = run_pop(task, 5)[0]
+    winner = run_pop(task, 21)[0]
     print('\nBest genome:\n{!s}'.format(winner))
 
     # Verify network output against training data.
