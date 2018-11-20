6cee3ab72d81a7be24f109a16f6b34d0e0ff6406
Nicholas Williams <nick5664@outlook.com>
2018-11-20 23:41:14 +0000

    im shit at p2p someone halp me

--- decent_trainer.py
+++ decent_trainer.py
@@ -17,7 +17,7 @@ from pureples.shared.substrate import Substrate
 from pureples.shared.visualize import draw_net
 from pureples.es_hyperneat.es_hyperneat import ESNetwork
 # Local
-class PurpleTrader(pkl="local_champ"):
+class DTrader(pkl="local_champ"):
     
     #needs to be initialized so as to allow for 62 outputs that return a coordinate
 
@@ -127,41 +127,5 @@ class PurpleTrader(pkl="local_champ"):
             network = ESNetwork(self.subStrate, cppn, self.params)
             net = network.create_phenotype_network_nd()
             g.fitness = self.evaluate(net, network, r_start)
-        
 
-
-# Create the population and run the XOR task by providing the above fitness function.
-def run_pop(task, gens):
-    pop = neat.population.Population(task.config)
-    stats = neat.statistics.StatisticsReporter()
-    pop.add_reporter(stats)
-    pop.add_reporter(neat.reporting.StdOutReporter(True))
-
-    winner = pop.run(task.eval_fitness, gens)
-    print("es trade god summoned")
-    return winner, stats
-
-
-# If run as script.
-if __name__ == '__main__':
-    task = PurpleTrader(5)
-    winner = run_pop(task, 21)[0]
-    print('\nBest genome:\n{!s}'.format(winner))
-
-    # Verify network output against training data.
-    print('\nOutput:')
-    cppn = neat.nn.FeedForwardNetwork.create(winner, task.config)
-    network = ESNetwork(task.subStrate, cppn, task.params)
-    with open('es_trade_god_cppn_3d.pkl', 'wb') as output:
-        pickle.dump(cppn, output)
-    #draw_net(cppn, filename="es_trade_god")
-    winner_net = network.create_phenotype_network_nd('dabestest.png')  # This will also draw winner_net.
-
-    # Save CPPN if wished reused and draw it to file.
-    #draw_net(cppn, filename="es_trade_god")
-
-    '''
-    for x in range(len(task.hs.hist_shaped[0])):
-        print(task.hs.hist_shaped[1][x][3],task.hs.hist_shaped[0][x][3])
-    '''
     
--- liq.py
+++ liq.py
@@ -33,4 +33,50 @@ config = {}
 
 @route("/")
 def best_pickle(request):
+    return "heres your pickle"
     
+    
+
+
+
+
+        
+
+
+# Create the population and run the XOR task by providing the above fitness function.
+def run_pop(task, gens):
+    pop = neat.population.Population(task.config)
+    stats = neat.statistics.StatisticsReporter()
+    pop.add_reporter(stats)
+    pop.add_reporter(neat.reporting.StdOutReporter(True))
+
+    winner = pop.run(task.eval_fitness, gens)
+    print("es trade god summoned")
+    return winner, stats
+
+
+# If run as script.
+if __name__ == '__main__':
+    task = PurpleTrader(5)
+    winner = run_pop(task, 21)[0]
+    print('\nBest genome:\n{!s}'.format(winner))
+
+    # Verify network output against training data.
+    print('\nOutput:')
+    cppn = neat.nn.FeedForwardNetwork.create(winner, task.config)
+    network = ESNetwork(task.subStrate, cppn, task.params)
+    with open('local_winner.pkl', 'wb') as output:
+        pickle.dump(cppn, output)
+    #draw_net(cppn, filename="es_trade_god")
+    winner_net = network.create_phenotype_network_nd('dabestest.png')  # This will also draw winner_net.
+    
+    # Save CPPN if wished reused and draw it to file.
+    #draw_net(cppn, filename="es_trade_god")
+
+    '''
+    for x in range(len(task.hs.hist_shaped[0])):
+        print(task.hs.hist_shaped[1][x][3],task.hs.hist_shaped[0][x][3])
+    '''
+
+
+
