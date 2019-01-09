bf3266c7c8e49548ad96a9704c658251403b460a
nick williams <nick5664@outlook.com>
2019-01-09 14:06:12 -0700

    vixifying in progress

--- hist_service.py
+++ hist_service.py
@@ -44,6 +44,9 @@ class HistWorker:
         histFiles = os.listdir(os.path.join(os.path.dirname(__file__), '../gdax'))
         return histFiles
 
+    def load_vix_hist_file(self):
+        return pd.read_csv('./data_files/vix-daily_csv.csv')
+
     def get_data_frame(self, fname):
         frame = pd.read_csv('./histories/'+fname) # timestamps will but used as index
         return frame
@@ -83,6 +86,14 @@ class HistWorker:
         df = df.drop('Unnamed: 0', 1).set_index("date")
         return moon.join(df, on="date")
 
+    def build_vix_frame(self):
+        df = self.load_vix_hist_file()
+        self.currentHists = df
+        df = df.drop('Date', 1)
+        df = (df - df.mean()) / (df.max() - df.min())
+        as_array = np.array(df)
+        self.hist_shaped = as_array
+    
     def pull_polo_live(self, lb):
         polo = Poloniex()
         coins = polo.returnTicker()
@@ -217,222 +228,7 @@ class HistWorker:
             main = main.join(df_list[i])
         return main
         '''
-import pickle
-import time
-import pandas as pd
-import requests
-import numpy as np
-from poloniex import Poloniex
-from datetime import date, timedelta, datetime
-import os
-#from ephemGravityWrapper import gravity as gbaby
-'''
-As can be expected by this point, you will notice that
-nothing done here has been done in the best possible way
-so feel STRONGLY ENCOURAGED TO FORK AND FIX/UPDATE/REFACTOR,
-also for the sake of not running computations for computations
-sake instead of calculating the actual gravitational pull we
-will just tack on a column of moon distances since its porportional
-to the gravitational pull
-and occurs at the same intervals
-'''
 
-'''
-the properties for histworker are set for the most part in combine frames which is called from the constructor
-'''
-class HistWorker:
-    look_back = 0
-    def __init__(self):
-        self.currentHists = {}
-        self.hist_shaped = {}
-        self.coin_dict = {}
-        #self.combine_frames()
-        self.look_back = 666
-        self.hist_full_size = 666*12
-        return
-
-    def get_hist_files(self):
-        histFiles = os.listdir(os.path.join(os.path.dirname(__file__), 'histories'))
-        return histFiles
-
-    def get_live_files(self):
-        histFiles = os.listdir(os.path.join(os.path.dirname(__file__), 'paper'))
-        return histFiles
-
-    def get_gdax_training_files(self):
-        histFiles = os.listdir(os.path.join(os.path.dirname(__file__), '../gdax'))
-        return histFiles
-
-    def get_data_frame(self, fname):
-        frame = pd.read_csv('./histories/'+fname) # timestamps will but used as index
-        return frame
-
-    def get_live_data_frame(self, fname):
-        frame = pd.read_csv('./paper/'+fname)
-        return frame
-
-    def get_file_as_frame(self, fname):
-        frame = pd.read_csv('../gdax/'+fname)
-        return frame
-
-    def get_file_symbol(self, f):
-        f = f.split("_", 2)
-        return f[1]
-    '''
-    def get_data_for_astro(self):
-        data = {}
-        dates = []
-        md = []
-        l_of_frame = len(self.currentHists['DASH']['date'])
-        for snoz in range(0, l_of_frame):
-            new_date = datetime.utcfromtimestamp((self.currentHists['DASH']['date'][snoz])).strftime('%Y-%m-%d  %H:%M:%S')
-            dates.append(self.currentHists['DASH']['date'][snoz])
-            md.append(gbaby.planet_dist(new_date, 'moon'))
-
-        data = {'date': dates, 'moon_dist': md}
-        data = pd.DataFrame.from_dict(data)
-        data.to_csv("moon_dists.txt", encoding="utf-8")
-        #data = data.join(self.currentHists['DASH'].set_index('date'), on='date', how="left").drop('Unnamed: 0', 1)
-        return data.head()
-    '''
-    def read_in_moon_data(self, df):
-        moon = pd.read_csv('./moon_dists.txt')
-        moon.set_index("date")
-        moon.drop("Unnamed: 0", 1)
-        df = df.drop('Unnamed: 0', 1).set_index("date")
-        return moon.join(df, on="date")
-
-    def pull_polo_live(self, lb):
-        polo = Poloniex()
-        coins = polo.returnTicker()
-        tickLen = '7200'
-        start = datetime.today() - timedelta(lb)
-        start = str(int(start.timestamp()))
-        for coin in coins:
-            if coin[:3] == 'BTC':
-                #print(coin)
-                hist = requests.get('https://poloniex.com/public?command=returnChartData&currencyPair='+coin+'&start='+start+'&end=9999999999&period='+tickLen)
-                h_frame = pd.DataFrame(hist.json())
-                frame = h_frame.copy()
-                frame['avg_vol_3'] = frame['volume'].rolling(3).mean()
-                frame['avg_vol_13'] = frame['volume'].rolling(13).mean()
-                frame['avg_vol_34'] = frame['volume'].rolling(34).mean()
-                frame['avg_close_3'] = frame['close'].rolling(3).mean()
-                frame['avg_close_13'] = frame['close'].rolling(13).mean()
-                frame['avg_close_34'] = frame['close'].rolling(34).mean()
-                frame.fillna(value=-99999, inplace=True)
-                print(coin + " written")
-                frame.to_csv("./paper/"+coin+"_hist.txt", encoding="utf-8")
-
-
-    def pull_polo(self):
-        polo = Poloniex()
-        coins = polo.returnTicker()
-        tickLen = '7200'
-        start = datetime.today() - timedelta(self.look_back)
-        start = str(int(start.timestamp()))
-        for coin in coins:
-            if coin[:3] == 'BTC':
-                hist = requests.get('https://poloniex.com/public?command=returnChartData&currencyPair='+coin+'&start='+start+'&end=9999999999&period='+tickLen)
-                try:
-                    h_frame = pd.DataFrame(hist.json())
-                    frame = h_frame.copy()
-                    frame['avg_vol_3'] = frame['volume'].rolling(3).mean()
-                    frame['avg_vol_13'] = frame['volume'].rolling(13).mean()
-                    frame['avg_vol_34'] = frame['volume'].rolling(34).mean()
-                    frame['avg_close_3'] = frame['close'].rolling(3).mean()
-                    frame['avg_close_13'] = frame['close'].rolling(13).mean()
-                    frame['avg_close_34'] = frame['close'].rolling(34).mean()
-                    frame = frame.fillna(value=-99999, inplace=True)
-                    print(frame.head())
-                    frame.to_csv("./histories/"+coin+"_hist.txt", encoding="utf-8")
-                except:
-                    print("error reading json")
-        #self.get_data_for_astro()
-
-
-    def combine_frames(self):
-        length = 7992
-        fileNames = self.get_hist_files()
-        coin_and_hist_index = 0
-        for x in range(0,len(fileNames)):
-            df = self.get_data_frame(fileNames[x])
-            col_prefix = self.get_file_symbol(fileNames[x])
-            #df.drop("Unnamed: 0", 1)
-            #df = self.read_in_moon_data(df)
-            df = df.drop("Unnamed: 0", 1)
-            #df.rename(columns = lambda x: col_prefix+'_'+x, inplace=True)
-
-            as_array = np.array(df)
-
-            #print(len(as_array))
-            if(len(as_array) == length):
-                self.currentHists[col_prefix] = df
-                df = (df - df.mean()) / (df.max() - df.min())
-                as_array=np.array(df)
-                self.hist_shaped[coin_and_hist_index] = as_array
-                self.coin_dict[coin_and_hist_index] = col_prefix
-                coin_and_hist_index += 1
-        self.hist_shaped = pd.Series(self.hist_shaped)
-        '''
-        main = df_list[0]
-        for i in range(1, len(df_list)):
-            main = main.join(df_list[i])
-        return main
-        '''
-
-    def combine_live_frames(self, length):
-        fileNames = self.get_live_files()
-        coin_and_hist_index = 0
-        for x in range(0,len(fileNames)):
-            df = self.get_live_data_frame(fileNames[x])
-            col_prefix = self.get_file_symbol(fileNames[x])
-            #df.drop("Unnamed: 0", 1)
-            #df = self.read_in_moon_data(df)
-            df = df.drop("Unnamed: 0", 1)
-            #df.rename(columns = lambda x: col_prefix+'_'+x, inplace=True)
-            as_array = np.array(df)
-            #print(len(as_array))
-            if(len(as_array) > length):
-                self.currentHists[col_prefix] = df
-                df = (df - df.mean()) / (df.max() - df.min())
-                as_array = np.array(df)
-                self.hist_shaped[coin_and_hist_index] = as_array
-                self.coin_dict[coin_and_hist_index] = col_prefix
-                coin_and_hist_index += 1
-        self.hist_shaped = pd.Series(self.hist_shaped)
-        '''
-        main = df_list[0]
-        for i in range(1, len(df_list)):
-            main = main.join(df_list[i])
-        return main
-        '''
-
-
-    def combine_live_usd_frames(self):
-        fileNames = self.get_gdax_training_files()
-        coin_and_hist_index = 0
-        for x in range(0,len(fileNames)):
-            df = self.get_file_as_frame(fileNames[x])
-            col_prefix = self.get_file_symbol(fileNames[x])
-            #df.drop("Unnamed: 0", 1)
-            #df = self.read_in_moon_data(df)
-            #df = df[::-1]
-            df = df[::-1]
-            self.currentHists[col_prefix] = df
-            df = df.drop('Symbol', 1)
-            df = df.drop("Date", 1)
-            df = (df - df.mean()) / (df.max() - df.min())
-            #df.rename(columns = lambda x: col_prefix+'_'+x, inplace=True)
-            as_array = np.array(df)
-            #print(len(as_array))
-            self.hist_shaped[coin_and_hist_index] = as_array
-            self.coin_dict[coin_and_hist_index] = col_prefix
-            coin_and_hist_index += 1
-        self.hist_shaped = pd.Series(self.hist_shaped)
-        '''
-        main = df_list[0]
-        for i in range(1, len(df_list)):
-            main = main.join(df_list[i])
-        return main
-        '''
+hs = HistWorker()
+hs.build_vix_frame()
+print(hs.hist_shaped.shape, hs.currentHists.head())
---
+++ vix_trainer.py
@@ -0,0 +1,173 @@
+
+### IMPORTS ###
+import random
+import sys, os
+from functools import partial
+from itertools import product
+from pytorch_neat.cppn import create_cppn
+# Libs
+import numpy as np
+from hist_service import HistWorker
+from crypto_evolution import CryptoFolio
+from random import randint, shuffle
+# Local
+import neat.nn
+import _pickle as pickle
+from pureples.shared.substrate import Substrate
+from pureples.shared.visualize import draw_net
+from pureples.es_hyperneat.es_hyperneat_torch import ESNetwork
+# Local
+class PurpleTrader:
+
+    #needs to be initialized so as to allow for 62 outputs that return a coordinate
+
+    # ES-HyperNEAT specific parameters.
+    params = {"initial_depth": 3,
+            "max_depth": 6,
+            "variance_threshold": 0.013,
+            "band_threshold": 0.013,
+            "iteration_level": 3,
+            "division_threshold": 0.013,
+            "max_weight": 5.0,
+            "activation": "tanh"}
+
+
+    # Config for CPPN.
+    config = neat.config.Config(neat.genome.DefaultGenome, neat.reproduction.DefaultReproduction,
+                                neat.species.DefaultSpeciesSet, neat.stagnation.DefaultStagnation,
+                                'config_trader')
+
+    start_idx = 0
+    highest_returns = 0
+    portfolio_list = []
+
+
+
+    in_shapes = []
+    out_shapes = []
+    def __init__(self, hist_depth):
+        self.hs = HistWorker()
+        self.hs.build_vix_frame()
+        self.hd = hist_depth
+        print(self.hs.currentHists.keys())
+        self.end_idx = len(self.hs.currentHists)
+        self.but_target = .1
+        self.inputs = self.hs.hist_shaped.shape[1]
+        self.outputs = 1
+        sign = 1
+        for ix in range(1,self.outputs+1):
+            sign = sign *-1
+            self.out_shapes.append((0.0-(sign*.005*ix), -1.0, -1.0))
+        self.out_shapes.append((0.0, 0.0, ))
+        self.subStrate = Substrate(self.in_shapes, self.out_shapes)
+        self.epoch_len = 21
+        #self.node_names = ['x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'weight']
+        self.leaf_names = []
+        #num_leafs = 2**(len(self.node_names)-1)//2
+        for l in range(len(self.in_shapes[0])):
+            self.leaf_names.append('leaf_one_'+str(l))
+            self.leaf_names.append('leaf_two_'+str(l))
+        #self.leaf_names.append('bias')
+    def set_portfolio_keys(self, folio):
+        for k in self.hs.currentHists.keys():
+            folio.ledger[k] = 0
+
+    def get_one_epoch_input(self,end_idx):
+        master_active = []
+        for x in range(0, self.hd):
+            active = []
+            #print(self.outputs)
+            for y in range(0, self.outputs):
+                try:
+                    sym_data = self.hs.hist_shaped[y][end_idx-x]
+                    #print(len(sym_data))
+                    active += sym_data.tolist()
+                except:
+                    print('error')
+            master_active.append(active)
+        #print(active)
+        return master_active
+
+    def evaluate(self, network, es, rand_start, verbose=False):
+        portfolio_start = .05
+        portfolio = CryptoFolio(portfolio_start, self.hs.coin_dict)
+        end_prices = {}
+        buys = 0
+        sells = 0
+        for z in range(rand_start, rand_start+self.epoch_len):
+            active = self.get_one_epoch_input(z)
+            network.reset()
+            for n in range(1, self.hd+1):
+                out = network.activate(active[self.hd-n])
+            #print(len(out))
+            rng = len(out)
+            #rng = iter(shuffle(rng))
+            for x in np.random.permutation(rng):
+                sym = self.hs.coin_dict[x]
+                #print(out[x])
+                #try:
+                if(out[0] < -.5):
+                    #print("selling")
+                    portfolio.sell_coin(sym, self.hs.currentHists[sym]['close'][z])
+                    #print("bought ", sym)
+                elif(out[0] > .5):
+                    #print("buying")
+                    portfolio.buy_coin(sym, self.hs.currentHists[sym]['close'][z])
+                    #print("sold ", sym)
+                #skip the hold case because we just dont buy or sell hehe
+                end_prices[sym] = self.hs.currentHists[sym]['close'][self.epoch_len+rand_start]
+        result_val = portfolio.get_total_btc_value(end_prices)
+        print(result_val[0], "buys: ", result_val[1], "sells: ", result_val[2])
+        ft = result_val[0]
+        if(ft == portfolio_start):
+            ft = portfolio_start/2
+        return ft
+
+    def solve(self, network):
+        return self.evaluate(network) >= self.highest_returns
+
+
+    def eval_fitness(self, genomes, config):
+        r_start = randint(0+self.hd, self.hs.hist_full_size - self.epoch_len)
+        for idx, g in genomes:
+            [cppn] = create_cppn(g, config, self.leaf_names, ['cppn_out'])
+            network = ESNetwork(self.subStrate, cppn, self.params)
+            net = network.create_phenotype_network_nd("current_net.png")
+            g.fitness = self.evaluate(net, network, r_start)
+
+
+
+# Create the population and run the XOR task by providing the above fitness function.
+def run_pop(task, gens):
+    pop = neat.population.Population(task.config)
+    stats = neat.statistics.StatisticsReporter()
+    pop.add_reporter(stats)
+    pop.add_reporter(neat.reporting.StdOutReporter(True))
+
+    winner = pop.run(task.eval_fitness, gens)
+    print("es trade god summoned")
+    return winner, stats
+
+
+# If run as script.
+if __name__ == '__main__':
+    task = PurpleTrader(13)
+    winner = run_pop(task, 10)[0]
+    print('\nBest genome:\n{!s}'.format(winner))
+
+    # Verify network output against training data.
+    print('\nOutput:')
+    cppn = neat.nn.FeedForwardNetwork.create(winner, task.config)
+    network = ESNetwork(task.subStrate, cppn, task.params)
+    with open('es_trade_god_cppn_3d.pkl', 'wb') as output:
+        pickle.dump(cppn, output)
+    #draw_net(cppn, filename="es_trade_god")
+    winner_net = network.create_phenotype_network_nd('dabestest.png')  # This will also draw winner_net.
+
+    # Save CPPN if wished reused and draw it to file.
+    #draw_net(cppn, filename="es_trade_god")
+
+    '''
+    for x in range(len(task.hs.hist_shaped[0])):
+        print(task.hs.hist_shaped[1][x][3],task.hs.hist_shaped[0][x][3])
+    '''
