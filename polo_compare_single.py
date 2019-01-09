b1cf9426c07d7c4dbdbb0b68ad1a094fc74ad60c
nick williams <nick5664@outlook.com>
2019-01-09 10:06:09 -0700

    yer

--- polo_compare_single.py
+++ polo_compare_single.py
@@ -1,10 +1,10 @@
-
 ### IMPORTS ###
 import random
 import sys, os
 from functools import partial
 from itertools import product
 from pytorch_neat.cppn import create_cppn
+import pandas as pd
 # Libs
 import numpy as np
 from hist_service import HistWorker
@@ -50,7 +50,7 @@ class PurpleTrader:
         self.hs.combine_frames()
         self.hd = hist_depth
         print(self.hs.currentHists.keys())
-        self.end_idx = len(self.hs.currentHists["ZEC"])
+        self.end_idx = len(self.hs.currentHists["ETH"])
         self.but_target = .1
         self.inputs = self.hs.hist_shaped[0].shape[1]
         self.outputs = 1
@@ -60,7 +60,7 @@ class PurpleTrader:
             self.in_shapes.append((0.0-(sign*.005*ix), -1.0, 0.0+(sign*.005*ix)))
         self.out_shapes.append((0.0, 1.0, 0.0))
         self.subStrate = Substrate(self.in_shapes, self.out_shapes)
-        self.epoch_len = 255
+        self.epoch_len = 144
         #self.node_names = ['x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'weight']
         self.leaf_names = []
         #num_leafs = 2**(len(self.node_names)-1)//2
@@ -87,7 +87,6 @@ class PurpleTrader:
             master_active.append(active)
         #print(active)
         return master_active
-
     def get_single_symbol_epoch(self, end_idx, symbol_idx):
         master_active = []
         for x in range(0, self.hd):
@@ -98,45 +97,79 @@ class PurpleTrader:
             except:
                 print('error')
         return master_active
+    def load_net(self, fname):
+        f = open(fname,'rb')
+        g = pickle.load(f)
+        f.close()
+        [the_cppn] = create_cppn(g, self.config, self.leaf_names, ['cppn_out'])
+        self.cppn = the_cppn
 
-    def evaluate(self, network, es, rand_start, g, verbose=False):
-        portfolio_start = .05
+    def run_champs(self):
+        genomes = os.listdir(os.path.join(os.path.dirname(__file__), 'champs_d2_single'))
+        fitness_data = {}
+        best_fitness = 0.0
+        for g_ix in range(len(genomes)):
+            genome = self.load_net('./champs_d2_single/'+genomes[g_ix])
+            start = self.hs.hist_full_size - self.epoch_len
+            network = ESNetwork(self.subStrate, self.cppn, self.params)
+            net = network.create_phenotype_network_nd('./champs_visualized2/genome_'+str(g_ix))
+            fitness = self.evaluate(net, network, start, g_ix, genomes[g_ix])
+            if fitness > best_fitness:
+                best_genome = genome
+
+    def evaluate(self, network, es, rand_start, g, p_name):
+        portfolio_start = 1.0
         portfolio = CryptoFolio(portfolio_start, self.hs.coin_dict)
         end_prices = {}
         buys = 0
         sells = 0
-        if(len(g.connections) > 0.0):
-            for z in range(rand_start, rand_start+self.epoch_len):
+        th = []
+        with open('./champs_hist2/trade_hist'+p_name + '.txt', 'w') as ft:
+            ft.write('date,symbol,type,amnt,price,current_balance \n')
+            for z in range(self.hd, self.hs.hist_full_size -1):
                 for x in np.random.permutation(self.outputs):
                     sym = self.hs.coin_dict[x]
                     active = self.get_single_symbol_epoch(z, x)
                     network.reset()
                     for n in range(1, self.hd+1):
                         out = network.activate(active[self.hd-n])
-                    #print(len(out))
-
-                    #print(out[x])
-                    #try:
+                    end_prices[sym] = self.hs.currentHists[sym]['close'][self.hs.hist_full_size-1]
+                    #rng = iter(shuffle(rng))
+                        #print(out[x])
+                        #try:
                     if(out[0] < -.5):
                         #print("selling")
-                        portfolio.sell_coin(sym, self.hs.currentHists[sym]['close'][z])
+                        did_sell = portfolio.sell_coin(sym, self.hs.currentHists[sym]['close'][z])
+                        if did_sell:
+                            ft.write(str(self.hs.currentHists[sym]['date'][z]) + ",")
+                            ft.write(sym +",")
+                            ft.write('sell,')
+                            ft.write(str(portfolio.ledger[sym])+",")
+                            ft.write(str(self.hs.currentHists[sym]['close'][z])+",")
+                            ft.write(str(portfolio.get_total_btc_value_no_sell(end_prices)[0])+ " \n")
                         #print("bought ", sym)
                     elif(out[0] > .5):
-                        #print("buying")
-                        portfolio.buy_coin(sym, self.hs.currentHists[sym]['close'][z])
+                        did_buy = portfolio.buy_coin(sym, self.hs.currentHists[sym]['close'][z])
+                        if did_buy:
+                            ft.write(str(self.hs.currentHists[sym]['date'][z]) + ",")
+                            ft.write(sym +",")
+                            ft.write('buy,')
+                            ft.write(str(portfolio.target_amount)+",")
+                            ft.write(str(self.hs.currentHists[sym]['close'][z])+",")
+                            ft.write(str(portfolio.get_total_btc_value_no_sell(end_prices)[0])+ " \n")
                         #print("sold ", sym)
-                    #skip the hold case because we just dont buy or sell hehe
-                    end_prices[sym] = self.hs.currentHists[sym]['close'][self.epoch_len+rand_start]
-            result_val = portfolio.get_total_btc_value(end_prices)
-            print(result_val[0], "buys: ", result_val[1], "sells: ", result_val[2])
-            ft = result_val[0]
-        else:
-            ft = 0.0
+                    #skip the hold case because we just dont buy or sell heh
+        result_val = portfolio.get_total_btc_value(end_prices)
+        print(result_val[0], "buys: ", result_val[1], "sells: ", result_val[2], p_name)
+        ft = result_val[0]
         return ft
 
     def solve(self, network):
         return self.evaluate(network) >= self.highest_returns
 
+    def report_back(self, portfolio, prices):
+        print(portfolio.get_total_btc_value(prices))
+        
     def trial_run(self):
         r_start = 0
         file = open("es_trade_god_cppn_3d.pkl",'rb')
@@ -153,8 +186,8 @@ class PurpleTrader:
         for idx, g in genomes:
             [cppn] = create_cppn(g, config, self.leaf_names, ['cppn_out'])
             network = ESNetwork(self.subStrate, cppn, self.params)
-            net = network.create_phenotype_network_nd()
-            g.fitness = self.evaluate(net, network, r_start, g)
+            net = network.create_phenotype_network_nd('current_net.png')
+            g.fitness = self.evaluate(net, network, r_start)
             if(g.fitness > fitter_val):
                 fitter = g
                 fitter_val = g.fitness
@@ -162,38 +195,7 @@ class PurpleTrader:
             pickle.dump(fitter, output)
         print("latest_saved")
 # Create the population and run the XOR task by providing the above fitness function.
-def run_pop(task, gens):
-    pop = neat.population.Population(task.config)
-    stats = neat.statistics.StatisticsReporter()
-    pop.add_reporter(stats)
-    pop.add_reporter(neat.reporting.StdOutReporter(True))
-
-    winner = pop.run(task.eval_fitness, gens)
-    print("es trade god summoned")
-    return winner, stats
 
 
-# If run as script.
-if __name__ == '__main__':
-    task = PurpleTrader(55)
-    #print(task.trial_run())
-    winner = run_pop(task, 34)[0]
-    print('\nBest genome:\n{!s}'.format(winner))
-
-    # Verify network output against training data.
-    print('\nOutput:')
-    [cppn] = create_cppn(winner, task.config, task.leaf_names, ['cppn_out'])
-    network = ESNetwork(task.subStrate, cppn, task.params)
-    with open('es_trade_god_cppn_3d.pkl', 'wb') as output:
-        pickle.dump(winner, output)
-    #draw_net(cppn, filename="es_trade_god")
-    winner_net = network.create_phenotype_network_nd('dabestest.png')  # This will also draw winner_net.
-
-    # Save CPPN if wished reused and draw it to file.
-    draw_net(cppn, filename="es_trade_god")
-
-
-    '''
-    for x in range(len(task.hs.hist_shaped[0])):
-        print(task.hs.hist_shaped[1][x][3],task.hs.hist_shaped[0][x][3])
-    '''
+pt = PurpleTrader(144)
+pt.run_champs()
